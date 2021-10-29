e2f27f1876eb9bed270889fa2dee04689d319b82
Sam Morris <sam@robots.org.uk>
2021-10-29 15:20:10 +0100

    Download EOS xml file

--- hp_networking_eos/__init__.py
+++ hp_networking_eos/__init__.py
@@ -1,2 +1,55 @@
+import json
+from pathlib import Path
+import xml.etree.ElementTree as ET
+from urllib.request import Request, urlopen
+import urllib.error
+
+
 def main(argv):
+
+    ensure_eos_xml()
+
+    tree = ET.parse('eos.xml')
+    print(tree.getroot())
+
     return 0
+
+
+def ensure_eos_xml():
+
+    eos_path = Path('eos.xml')
+    headers_path = Path('eos.xml-headers.json')
+
+    r = Request('https://techlibrary.hpe.com/data/xml/eos/eos.xml')
+
+    if headers_path.exists():
+        with open(headers_path, 'r') as f:
+            try:
+                old_headers = json.load(f)
+            except Exception as e:
+                pass
+            else:
+                if 'Last-Modified' in old_headers:
+                    r.add_header('If-Modified-Since', old_headers['Last-Modified'])
+
+    try:
+        with urlopen(r) as u:
+            if u.status == 304:
+                assert eos_path.exists()
+                return
+
+            with open(eos_path, 'wb') as f:
+                f.write(u.read())
+
+            with open(headers_path, 'w') as f:
+                json.dump(dict(u.headers.items()), f)
+
+    except urllib.error.HTTPError as e:
+        if e.code == 304:
+            if not eos_path.exists():
+                headers_path.unlink()
+                ensure_eos_xml()
+        else:
+            raise
+
+
